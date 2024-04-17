760c51b5fbbef7a87b6f8bebaec4d0bf3483f998
TiagoNelvo <ti.609@hotmail.com>
2024-04-17 17:20:48 -0300

    introdução curso java

---
+++ src/academy/devdojo/maratonajava/Aula02TiposPrimitivos.java
@@ -0,0 +1,4 @@
+package academy.devdojo.maratonajava;
+
+public class Aula02TiposPrimitivos {
+}
---
+++ src/academy/devdojo/maratonajava/OlaDevDojo.java
@@ -0,0 +1,19 @@
+package academy.devdojo.maratonajava;
+
+/**
+ * Isso é um comentário<b>javadock</b>
+ */
+public class OlaDevDojo {
+    public static void main(String[] args){
+        // Isso é um comentário de uma linha
+        /*
+        Isso é um comentário
+        de multiplas
+         linhas
+         */
+
+        System.out.println("KA-ME-HA-ME-HAA");
+
+    }
+
+}
