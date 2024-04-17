c61c49319f6010407912272cd139ed612ce57735
TiagoNelvo <ti.609@hotmail.com>
2024-04-17 19:06:23 -0300

    Tipos primitivos

--- src/academy/devdojo/maratonajava/Aula02TiposPrimitivos.java
+++ src/academy/devdojo/maratonajava/Aula02TiposPrimitivos.java
@@ -4,7 +4,21 @@ public class Aula02TiposPrimitivos {
     public static void main(String[] args) {
         // int, double, float, char, byte, short, long, boolean
         int idade = 10;
+        long numeroGrande = 100000;
+        double salarioDouble = 2000;
+        float salarioFloat = 2500;
+        byte idadeByte = 10;
+        short idadeShort = 10;
+        boolean verdadeiro = true;
+        boolean falso = false;
+        char caractere = 'M';
+        String nome = "Goku";
+        
         System.out.println("A idade é " + idade);
+        System.out.println(falso);
+        System.out.println("char "+caractere);
+        System.out.println(numeroGrande);
+        System.out.println("Oi meu nome é " + nome);
 
     }
 }
