134b5f94daec13e7a177b6285080fdc5bac93095
TiagoNelvo <ti.609@hotmail.com>
2024-04-19 19:02:29 -0300

    Aula06 Repetição

--- src/academy/devdojo/maratonajava/introducao/Aula06EstruturasDeRepeticao01.java
+++ src/academy/devdojo/maratonajava/introducao/Aula06EstruturasDeRepeticao01.java
@@ -1,2 +1,13 @@
-package academy.devdojo.maratonajava.introducao;public class Aula06EstruturasDeRepeticao01 {
+package academy.devdojo.maratonajava.introducao;
+
+public class Aula06EstruturasDeRepeticao01 {
+    public static void main(String[] args) {
+        //while, do while. for
+        int count = 0;
+        while(count < 10){
+            System.out.println(count);
+            count++;
+
+        }
+    }
 }
