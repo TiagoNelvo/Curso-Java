60396ceb4923725c98b3a53415089e4995d44e84
TiagoNelvo <ti.609@hotmail.com>
2024-04-19 16:59:43 -0300

    Operador Ternário

--- src/academy/devdojo/maratonajava/introducao/Aula05EstruturasCondicionais03.java
+++ src/academy/devdojo/maratonajava/introducao/Aula05EstruturasCondicionais03.java
@@ -2,6 +2,24 @@ package academy.devdojo.maratonajava.introducao;
 
 public class Aula05EstruturasCondicionais03 {
     public static void main(String[] args) {
-        double salario =
+        double salario = 3000;
+        String mensagemDoar = "Eu vou doar 500 pro DevDojo";
+        String mensagemNaoDoar = "Ainda não tenho condições, mas vou ter!";
+        /*String resultado;
+        if (salario > 5000){
+            resultado = mensagemDoar;
+        }else{
+            resultado = mensagemNaoDoar;
+        }*/
+        // (condição) ? verdadeiro : falso
+        String resultado = salario > 5000 ? mensagemDoar : mensagemNaoDoar;
+
+        System.out.println(resultado);
+        //System.out.println(salario > 5000 ? mensagemDoar : mensagemNaoDoar);
+
+        int idade = 18;
+        String categoria;
+        categoria = idade < 15 ? "Categoria Infantil" : idade >= 15 && idade < 18 ? "Categoria juvenil" : "Categoria Adulto";
+        System.out.println(categoria);
     }
 }
