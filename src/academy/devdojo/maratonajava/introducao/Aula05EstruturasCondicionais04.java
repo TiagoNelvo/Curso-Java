1dce6ac8f2e0bfd4afcad58de0dafe4572916e45
TiagoNelvo <ti.609@hotmail.com>
2024-04-19 17:52:00 -0300

    exercicio aula04

--- src/academy/devdojo/maratonajava/introducao/Aula05EstruturasCondicionais04.java
+++ src/academy/devdojo/maratonajava/introducao/Aula05EstruturasCondicionais04.java
@@ -5,18 +5,18 @@ public class Aula05EstruturasCondicionais04 {
         /* € 0  € 34,712  9.70%
            € 34,713 € 68,507  37.35%
          €68,508  49.50% */
-        double salarioAnual = 45000;
+        double salarioAnual = 70000;
         double primeiraFaixa = 9.70 / 100;
         double segundaFaixa = 37.35 / 100;
         double terceiraFaixa = 49.50 / 100;
         double valorImposto;
         if(salarioAnual <= 34712){
-            salarioAnual = salarioAnual * primeiraFaixa;
+            valorImposto = salarioAnual * primeiraFaixa;
         }else if(salarioAnual >= 34713 && salarioAnual <= 68507){
-            salarioAnual = salarioAnual * segundaFaixa;
+            valorImposto = salarioAnual * segundaFaixa;
         }else{
-            salarioAnual = salarioAnual * terceiraFaixa;
+            valorImposto = salarioAnual * terceiraFaixa;
         }
-        System.out.println(salarioAnual);
+        System.out.println(valorImposto);
     }
 }
