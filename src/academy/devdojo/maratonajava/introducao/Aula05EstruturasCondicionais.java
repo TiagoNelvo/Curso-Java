32a1c726e37dda87373a7db10daa6f5ba37d65f8
TiagoNelvo <ti.609@hotmail.com>
2024-04-19 15:20:15 -0300

    Aula 05

--- src/academy/devdojo/maratonajava/introducao/Aula05EstruturasCondicionais.java
+++ src/academy/devdojo/maratonajava/introducao/Aula05EstruturasCondicionais.java
@@ -3,12 +3,18 @@ package academy.devdojo.maratonajava.introducao;
 public class Aula05EstruturasCondicionais {
     public static void main(String[] args) {
         int idade = 15;
-
-        if(idade >= 18 ){
+        boolean isAutorizadoComprarBebida = idade >= 18;
+        if (isAutorizadoComprarBebida) {
             System.out.println("Autorizado a comprar bebida alcólica");
-
-
         }
-        System.out.println("Não autorizado a comprar bebida alcólica");
+        //if (isAutorizadoComprarBebida == false) {
+        if (!isAutorizadoComprarBebida) {
+        System.out.println("Não está autorizado a comprar bebida alcólica");
+        }
+        boolean c = false;
+        if(c == true){
+            System.out.println("Dentro de algo que nunca deve ser feito");
+        }
+
     }
 }
