23ee276c3364da7d82d39b8c2a8986a64553eb86
TiagoNelvo <ti.609@hotmail.com>
2024-04-23 19:29:00 -0300

    Aula08ArraysMultidimensionais01

--- src/academy/devdojo/maratonajava/introducao/Aula08ArraysMultidimensionais01.java
+++ src/academy/devdojo/maratonajava/introducao/Aula08ArraysMultidimensionais01.java
@@ -12,10 +12,18 @@ public class Aula08ArraysMultidimensionais01 {
         dias [1][0] = 31;
         dias [1][1] = 28;
         dias [1][2] = 31;
-        //System.out.println(dias[0[0]]);
+
         for (int i = 0; i < dias.length; i++){
-            for (int j = 0; j < dias[0].length; j++){
+            for (int j = 0; j < dias[i].length; j++){
                 System.out.println(dias[i][j]);
+            }
+        }
+
+        System.out.println("*****************");
+        
+        for (int[] arrBase: dias){
+            for (int num: arrBase){
+                System.out.println(num);
             }
         }
     }
